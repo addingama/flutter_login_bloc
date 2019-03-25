4f649382bda4f81526c29af6e16f38468041a3a7
Addin Gama Bertaqwa <punyagama@gmail.com>
2019-03-25 21:19:31 +0800

    Receive value from stream in Bloc

--- lib/src/blocs/bloc.dart
+++ lib/src/blocs/bloc.dart
@@ -4,8 +4,10 @@ import 'package:rxdart/rxdart.dart';
 
 
 class Bloc extends Object with Validators {
-  final _email = StreamController<String>.broadcast();
-  final _password = StreamController<String>.broadcast();
+
+  // BehaviorSubject is StreamController that keep reference to the last event, by default is broadcast stream
+  final _email = BehaviorSubject<String>();
+  final _password = BehaviorSubject<String>();
 
 
   // Getter on dart using keyword `get`
@@ -18,6 +20,15 @@ class Bloc extends Object with Validators {
   Stream<String> get email => _email.stream.transform(validateEmail);
   Stream<String> get password => _password.stream.transform(validatePassword);
   Stream<bool> get submitValid => Observable.combineLatest2(email, password, (e, p) => true);
+
+
+  submit() {
+    final validEmail = _email.value;
+    final validPassword =_password.value;
+
+    print('Email is $validEmail');
+    print('Password is $validPassword');
+  }
 
   // clean up function
   dispose () {
